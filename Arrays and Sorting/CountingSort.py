1adc8a68835efa60a042b1e7f6249480cc744334
Jesse McNary <mcnaryjesse@gmail.com>
2024-06-14 14:01:36 -0500

    Added Counting Sort

--- Arrays and Sorting/CountingSort.py
+++ Arrays and Sorting/CountingSort.py
@@ -2,31 +2,39 @@ import random
 
 
 
+def CountingSort(arr):
 
+    max_val = max(arr)
+    count = [0] * (max_val + 1)
 
+    while len(arr) > 0:
+        num = arr.pop(0)
+        count[num] += 1
 
+    for i in range(len(count)):
+        while count[i] > 0:
+            arr.append(i)
+            count[i] -= 1
 
-
-
-
-
-
-
+    return arr
 
 
 
 
 def main():
-    arrList = []
-    for i in range(10):
-        for i in range(10):
+    arrList = [[],[],[],[],[],[],[],[],[],[]]
+    print("Unsorted Arr")
+    for i in range(0,10):
+        for i in range(0,10):
             arrList[i].append(random.randrange(0,100))
-        print(arrList[i])
+        #print(arrList[i])
+    for i in arrList:
+        print(i)
 
     sortedArr = []
     for i in arrList:
-        sortedArr.append(func(i))
-    
+        sortedArr.append(CountingSort(i))
+    print("Sorted Arr")
     for i in sortedArr:
         print(i)
     
