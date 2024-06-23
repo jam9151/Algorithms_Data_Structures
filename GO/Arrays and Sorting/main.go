e015b2a1a4645eae904813f7294b1f73230152fd
Jesse McNary <mcnaryjesse@gmail.com>
2024-06-22 22:18:05 -0500

    Added GO functions

---
+++ GO/Arrays and Sorting/main.go
@@ -0,0 +1,25 @@
+/*
+Notes
+	1. Var vs :=. := can only be used inside functions
+	2. %d for Decimal, %x for Hex, %o for Octal, %b = Binary
+	3. Printing strings is as simple as fmt.println(string)
+	4. Printing array is as simple as fmt.Println(Array)
+	5. Datatypes = int,string,float64,bool
+	6.
+
+
+*/
+
+package main
+
+import (
+	"fmt"
+)
+
+var DEBUG int = 0
+
+func main() {
+
+	fmt.Println()
+
+}
